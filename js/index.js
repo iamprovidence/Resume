63c1f9ac89733a4e9a5134d7687a3deac746b67f
iamprovidence <orleon.nilkford@gmail.com>
2019-05-11 17:12:22 +0300

    Add books section close#4

--- js/index.js
+++ js/index.js
@@ -14,7 +14,13 @@ if (!String.prototype.format)
 
 $(document).ready(function()
 {
+    my_skills_data();
+    books_read_data();
+});
 
+// SKILLS
+function my_skills_data()
+{
     var data_array = 
     [
         {
@@ -39,20 +45,68 @@ $(document).ready(function()
         },
     ];
 
-var timeline = $("#timeline");
-var template = $("#template-block").text();
+    var timeline = $("#timeline");
+    var template = $("#timeline-template-block").text();
 
-var isLeft = true;
-for (var i = 0; i < data_array.length; ++i)
-{
-    var technologies = "";
-    for (var j = 0; j < data_array[i].technologies.length; ++j)
+    var isLeft = true;
+    for (var i = 0; i < data_array.length; ++i)
     {
-        technologies += `<li>${data_array[i].technologies[j]}</li>`;
-    }
+        var technologies = "";
+        for (var j = 0; j < data_array[i].technologies.length; ++j)
+        {
+            technologies += `<li>${data_array[i].technologies[j]}</li>`;
+        }
 
-    // append block
-    timeline.append(template.format(isLeft ? "left" : "right", data_array[i].year, technologies));
-    isLeft =! isLeft;
+        // append block
+        timeline.append(template.format(isLeft ? "left" : "right", data_array[i].year, technologies));
+        isLeft =! isLeft;
+    }
 }
-});
+
+// BOOKS
+function books_read_data()
+{
+    var data_array =
+    [
+        {
+            css_class: 'pink',
+            language: 'C++',
+            book_name: 'Primer plus',
+            author: 'Stephen Prata'
+        },
+        {
+            css_class: 'yellow',
+            language: 'JavaScript',
+            book_name: 'The Definitive Guide',
+            author: 'David Flanagan'
+        },
+        {
+            css_class: 'blue',
+            language: 'PHP',
+            book_name: 'Learning PHP, MySQL, & JavaScript',
+            author: 'Robin Nickson'
+        },
+        {
+            css_class: 'green',
+            language: 'C#',
+            book_name: 'Professional C# 7 and .NET Core 2.0',
+            author: 'Christian Nagel'
+        },
+        {
+            css_class: 'red',
+            language: 'ASP',
+            book_name: 'Pro ASP.Net Core MVC 2',
+            author: 'Adam Freeman'
+        },
+        
+    ];
+
+    var books_list = $("#books-list");
+    var template = $("#books-template-block").text();
+
+    // add data
+    for (var i = 0; i < data_array.length; ++i)
+    {
+        books_list.append(template.format(data_array[i].css_class, data_array[i].language, data_array[i].book_name, data_array[i].author));
+    }
+}
